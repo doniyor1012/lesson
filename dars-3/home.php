0a30ee4d136a235acf773d516c0903ae811f7397
begin <doniyoraxmedov522@gmail.com>
2023-05-05 22:45:27 +0500

    www

---
+++ dars-3/home.php
@@ -0,0 +1,15 @@
+
+<?php
+$a = 5;
+$b = 2;
+
+if ($a!=0 && $b!=0){
+    echo $a+$b." ";
+    echo $a*$b." ";
+    echo $a*$a." ";
+    echo $b*$b." ";
+}
+else {
+    echo "a va b sonlar nolga teng bo'lmasligi kerak";
+}
+?>
---
+++ dars-3/savol.php
@@ -0,0 +1,8 @@
+<?php
+$n=7;
+$cnt=0;
+for ($i=1;$i<$n;$i++){
+    if ($n%$i==0){$cnt++;}
+}if($cnt==2){'tub';}else{'tubemas';}
+echo $n;
+?>
