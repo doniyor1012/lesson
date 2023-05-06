8fa5ab340d13c95f3ddeb346afa454c678208a5d
doniyor <doniyoraxmedov522@gmail.com>
2023-05-07 01:08:11 +0500

    www

---
+++ dars-3/boolean12.php
@@ -0,0 +1,11 @@
+<?php
+// Uch xonali son berilgan. Jumlani rostlikka tekshiring: “Ushbu sonning barcha raqamlari xar hil ‘’
+$a=3;
+$b=3;
+$c=4;
+if ($a<$b && $b<$c){
+    echo "sonlar xar xil";
+}else{
+    echo "sonlar xar xil emas";
+}
+?>
