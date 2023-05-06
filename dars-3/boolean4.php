f359ac17c7ee731e688390caa474be08a46bed32
doniyor <doniyoraxmedov522@gmail.com>
2023-05-06 17:48:01 +0500

    www

---
+++ dars-3/boolean4.php
@@ -0,0 +1,13 @@
+<?php
+//Uchta A, V, S butun sonlar berilgan. Jumlani rostlikka tekshiring:
+// “B soni A va S sonlari orasida yotadi”
+$a = 7;
+$c = 6;
+$b = 5;
+if ($b>$a && $b<$c){
+    echo "b soni a va c orasida";
+}
+else{
+    echo "b soni a va c orasida emas";
+}
+?>
