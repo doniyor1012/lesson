24a852b6981899a405a5c90ad3f0ee46107f0b43
doniyor <doniyoraxmedov522@gmail.com>
2023-05-07 01:32:22 +0500

    www

---
+++ dars-3/boolean14.php
@@ -0,0 +1,12 @@
+<?php
+//Uch xonali son berilgan. Jumlani rostlikka tekshiring: “Ushbu sonni chapdan o’qiganda ham, o’ngdan o'qiganda ham bir xil".
+$a=2;
+$b=1;
+$c=1;
+if ($a==$b && $b==$c){
+    echo "o'ngdan chapga qarab o'qilsa ham  <br>   ";
+echo "    chapdan o'nga qarab o'qilsa bir xil ";
+}else {
+    echo "bir xil emas";
+}
+?>
