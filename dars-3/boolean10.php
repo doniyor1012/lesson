ba850587cd96ea8e9a383314fdb0b4ba396354b8
doniyor <doniyoraxmedov522@gmail.com>
2023-05-07 00:50:19 +0500

    www

---
+++ dars-3/boolean10.php
@@ -0,0 +1,13 @@
+<?php
+// Musbat butun son berilgan. Jumlani rostlikka tekshiring: “Berilgan son ikki xonali juft son”.
+// Musbat butun son berilgan. Jumlani rostlikka tekshiring: “Berilgan son uch xonali toq".
+$a=2;
+$b=4;
+$c=6;
+if ($a % 2!=0 || $b% 2!=0 || $c % 2!=0){
+    echo "berilgan son 3 xonali toq son";
+}
+else {
+    echo " berilgan son 3 xonali toq son emas";
+}
+?>
