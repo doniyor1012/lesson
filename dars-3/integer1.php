c6371afbc6a6741351aeb52912c37be8fd8bdda8
doniyor <doniyoraxmedov522@gmail.com>
2023-05-07 11:35:36 +0500

    www

---
+++ dars-3/integer1.php
@@ -0,0 +1,23 @@
+<?php
+//integer Uzunlik L santimetrda berilgan. Undagi to'liq metrlar sonini aniqlovchi programma tuzilsin. (1m=100sm)
+   /* $l= 300 ;
+$a=(100/1);
+$b= $l/$a;
+var_dump($b);
+*/
+
+//. Og'irlik M kilogramda berilgan. Undagi to'liq tonnalar sonini aniqlovchi programme tuzilsin. (1t=1000kg)
+   /* $m=3000;
+    $a=1000;
+    $son=intval($m/$a);
+ echo $son;*/
+
+//Faylning hajmi baytlarda berilgan. Bo'lib butunni olish operatsiyasidan foydalanib
+// fayl hajmining to’liq kilobaytlarda ifodalovchi programme tuzilsin. (1Kb=1024 b)
+$a=10240;
+$b=1024;
+
+$son=intval($a/$b);
+echo $son;
+
+?>
