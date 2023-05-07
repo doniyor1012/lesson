83408341ca872ebee23b9f729fd0f5399965164b
doniyor <doniyoraxmedov522@gmail.com>
2023-05-07 15:48:39 +0500

    www

--- dars-3/integer3.php
+++ dars-3/integer3.php
@@ -1,7 +1,7 @@
 <?php
 // 1 Uch xonali son berilgan. Oldin uni birliklar xonasidagi raqamni
 // so'ng o'nliklar xonasidagi raqamni chiqaruvchi programma tuzilsin.
-$a=1;
+    /*a=1;
 $b=2;
 $c=14;
 if ($a<9 && $b<9 && $c<9){
@@ -11,5 +11,16 @@ if ($a<9 && $b<9 && $c<9){
 }
 else {
     echo "A vaB sonlar har xil";
-}
+}   */
+
+// 2 Uch xonali son berilgan. Uning raqamlarini teskari tartibda yozishdan
+//hosil bo'lgan sonni aniqlovchi program tuzilsin.
+ /* $a=1234;
+echo strrev($a);  */
+
+// 3 Uch xonali son berilgan. L ning chapdan birinchi raqamini o’chirib
+// o'ng tarafiga yozishdan hosil bo'lgan sonni aniqlovchi programma tuzilsin
+$l=12345;
+echo substr($l,1)
+
 ?>
