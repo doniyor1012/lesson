e040fc7a4a6563eb9906a621e01f2369c521d039
doniyor <doniyoraxmedov522@gmail.com>
2023-05-07 15:07:10 +0500

    www

---
+++ dars-3/integer2.php
@@ -0,0 +1,42 @@
+<?php
+// 1 Uch xonali son berilgan. Uning yuzlar xonasidagi raqamini aniqlovchi programma tuzilsin
+  /*  $a=111;
+    $b=111;
+    if ($a>99 && $b>99){
+    echo "A vaB sonlar yuzlik xonasidagi son" ;
+
+    }
+    else{
+    echo "a va b sonlar yuzlik xonasidagi son emas";
+    }
+*/
+
+// 2 Ikki xonali son berilgan. Oldin uning o’nliklar xonasidagi raqamni,
+// so'ng birlar xonasidagi raqamni chiqaruvchi programma tuzilsin.
+  /*  $a=1;
+    $b=1;
+if ($a>9 && $b>9){
+    echo "A va B sonlar o'nlik xonasidagi son ";
+}elseif ($a<9&& $b<9){
+    echo "A va B sonlar birlik ";
+}
+else{
+    echo "A vaB sonlar o'nlik xonasidagi son emas";
+}   */
+//.3 Ikki xonali son berilgan. Uning raqamlari yig'indisini aniqlovchi programma tuzilsin.
+
+  /*  $a=4;
+    $b=3;
+    $son=intval($a+$b);
+    echo $son;  */
+
+// 4 Ikki xonali son berilgan. Uning raqamlari o’mini
+//  almashtirishdan hosil bo'lgan sonni aniqlovchi programma tuzilsin.
+    $a=4;
+    $b=7;
+    $c=$a;
+    $a=$b;
+    $b=$c;
+    echo $a."";
+    echo $b."";
+?>
