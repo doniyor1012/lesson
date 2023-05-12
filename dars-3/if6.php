1c8eb013c91bec22eeb8dea0233d894b3485e65c
doniyor <doniyoraxmedov522@gmail.com>
2023-05-12 12:30:51 +0500

    zzz

---
+++ dars-3/if6.php
@@ -0,0 +1,14 @@
+<?php
+//Uchta son berilgan. Shu sonlarni kichigini aniqlovchi programma tuzilsin.
+    $a=9;
+    $b=5;
+    $c=7;
+
+if ($a<$b && $a<$c){
+    echo $a;
+}elseif ($b<$a&&$b<$c){
+    echo $b;
+}elseif ($c<$a&& $c<$b){
+    echo $c;
+}
+
