3d469f1905d421c9edf0d640329a39bacc84505e
doniyor <doniyoraxmedov522@gmail.com>
2023-05-14 02:29:04 +0500

    eeee

---
+++ dars-3/begin17.php
@@ -0,0 +1,10 @@
+<?php
+//Birinchi avtomabilning tezligi  V1 km / soat   ikkinchisiniki,
+// V2 km / soat   ular orasidagi masofa   S. Ular biri-biridan uzoqlasha boshlasa
+// T   vaqtdan keyin ular orasidagi masofani aniqlaydigan programma tuzilsin
+$v1=80;
+$v2=50;
+$a=25;
+$s=$v1-$v2;
+$t=$a*$s;
+echo $t;
