cfb0a06af277d28d5929a63c96abeb021855289b
doniyor <doniyoraxmedov522@gmail.com>
2023-05-14 02:25:17 +0500

    eeee

---
+++ dars-3/begin16.php
@@ -0,0 +1,12 @@
+<?php
+//Qayiqning tezligi V km / soat   daryo oqimining tezligi  U km / soat, (V>U)
+// Qayiqning daryo    oqimi bo'yicha harakatlanish vaqti T1   ,
+// oqimga qarshi T2 Qayiqni yurgan S yo'lini aniqlovchi programma tuzilsin.
+$v=70;
+$u=40;
+$t1=50;
+$t2=30;
+$a=$t1-$t2;
+$b=$v+$u;
+$c=$a+$b;
+echo $c;
