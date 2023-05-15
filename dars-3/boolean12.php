79b95b5388f4dc5106239fbcafc985836e0a0c56
doniyor <doniyoraxmedov522@gmail.com>
2023-05-15 12:10:46 +0500

    eeee

--- dars-3/boolean12.php
+++ dars-3/boolean12.php
@@ -1,11 +1,6 @@
 <?php
 // Uch xonali son berilgan. Jumlani rostlikka tekshiring: “Ushbu sonning barcha raqamlari xar hil ‘’
-$a=3;
-$b=3;
-$c=4;
-if ($a<$b && $b<$c){
-    echo "sonlar xar xil";
-}else{
-    echo "sonlar xar xil emas";
-}
-?>
+$a=123;
+$b=$a%10; $a=intval($a/10);
+$c=$a%10; $a=intval($a/10);
+$d=$a%10; $a=intval($a/10);
---
+++ dars-3/integer6.php
@@ -0,0 +1,9 @@
+<?php
+//Qaysidir yil berilgan. Berilgan yilning qaysi yuz yillikka kirishini aniqlovchi
+// programma tuzilsin. (Masalan: 20 - yuz yillikning boshi 1901 yil).
+$a=2001;
+$b=100;
+$c=intval($a/$b)+1;
+echo $c."-asr";
+
+
