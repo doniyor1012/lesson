df779ff1ecef05bbf5a0be6aa1bad36221fed26b
doniyor <doniyoraxmedov522@gmail.com>
2023-05-15 12:12:38 +0500

    eeee

--- dars-3/integer5.php
+++ dars-3/integer5.php
@@ -2,8 +2,8 @@
 //. O-yakshanba, 1-dushanba, 2-seshanba, 3- chorshanba, 4-payshanba, 5-juma, 6-shanba
 //. 1-365 oraliqda yotuvchi K soni berilgan. Agar 1-yanvar dushanba bo’lsa, kiritilgan
 // K- kun haftaning qaysi kuniga to'g'ri kelishini aniqlovchi programma tuzilsin.
-$n=1;
-$k=7;
-$x=intval($k/$n);
+$n=4;
+$k=40;
+$x=intval($k/$n)%7;
 echo $x;
 
