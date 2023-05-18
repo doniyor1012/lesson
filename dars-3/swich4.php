accdb3f086b5cb0e8f6f96df959b6144386ed84d
doniyor <doniyoraxmedov522@gmail.com>
2023-05-18 23:54:40 +0500

    eeee

---
+++ dars-3/swich4.php
@@ -0,0 +1,19 @@
+<?php
+//Oy raqami berilgan. Shu oyda nechta kun borligini aniqlovchi programma tuzilsin.
+$a=12;
+$res="";
+switch ($a){
+    case 1:$res="30 kun";break;
+    case 2:$res="28 kun , 4 yilda 1 marta 29 kun";break;
+    case 3:$res="31 kun";break;
+    case 4:$res="31 kun";break;
+    case 5:$res="30 kun";break;
+    case 6:$res="30 kun";break;
+    case 7:$res="30 kun";break;
+    case 8:$res="30 kun";break;
+    case 9:$res="30 kun";break;
+    case 10:$res="31 kun";break;
+    case 11:$res="31 kun";break;
+    case 12:$res="31 kun";break;
+    default:$res="xato yozilgan";break;
+}echo $res;
