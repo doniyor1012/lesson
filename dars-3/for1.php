f030f795260180a800065e12e296d090d484d01b
doniyor <doniyoraxmedov522@gmail.com>
2023-05-19 12:09:02 +0500

    hhhh

---
+++ dars-3/for1.php
@@ -0,0 +1,8 @@
+<?php
+//. k va n butun sonlari berilgan (n > 0). k sonini n marta chiqaruvchi programma tuzilsin.
+
+$n=15;
+$k=0;
+for (;$k<$n;$k++){
+echo $k;
+}
---
+++ dars-3/swich8.php
@@ -0,0 +1,9 @@
+<?php
+//Sanani bildiruvchi ikkita butun son berilgan D (kun) va M (oy).
+// (Kabisa bo'lmagan yil sanasi kiritiladi). Berilgan sanani ifodalovchi programma tuzilsin
+//. Kabisa yilida 366 kun, kabisa bo’lmagan yilda 365 kun bor bo’ladi.
+
+$n=30;
+$kun=date(D);
+$oy=date(M);
+
