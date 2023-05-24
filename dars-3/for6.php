57cad821332a395eb8b3d26c0377ec6038e8c09b
doniyor <doniyoraxmedov522@gmail.com>
2023-05-25 01:52:09 +0500

    ppppp

---
+++ dars-3/for6.php
@@ -0,0 +1,8 @@
+<?php
+//. Bir kg konfetning narxi berilgan (haqiqiy son). 1.2, 1.4, ..., 2 kg
+// konfetni narxini chiqaruvchi programma tuzilsin.
+$a=10;
+for ($i=0;$i<$a;$i++){
+    $b=($i/10);
+        $c=$i*$a;
+}echo $b."kg narxi=".' ' .$c.' '."so'm";
