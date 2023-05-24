68f0e4c2a52b07d9596e145bf6a32a0af8f2f85f
doniyor <doniyoraxmedov522@gmail.com>
2023-05-25 01:20:27 +0500

    ppppp

---
+++ dars-3/for14.php
@@ -0,0 +1,12 @@
+<?php
+//. n butun soni berilgan (n > 0). Shu sonning kvadratini quyidagi formula asosida hisoblovchi
+// programma tuzilsin. n2= 1 +3 + 5 + ... + (2*n -1) har bir
+// qo’shiluvchidan keyin natijani ekranga chiqarib boring. Natijda ekranda
+// 1 dan n gacha bo’lgan sonlar kvadrati chiqariladi
+$n=2;
+$s=1;
+for ($i=1; $i <$n ; $i++) { 
+$s+=pow($n,2)*(2*$i-1);
+}echo "$s\n";
+
+?>
